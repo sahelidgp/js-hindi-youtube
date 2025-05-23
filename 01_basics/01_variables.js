9722d7505e31482ee749f4c6b4cdc5141aeda1e2
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-05-23 08:32:43 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "smahantyprl20@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+//accountId = 6789 //not allowed
+
+let accountState;
+
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
+accountEmail = "sm.23cs8107@nitdgp.ac.in"
+accountCity  = "Purulia"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+Prefer not to use var 
+because of issue of block scope and functional code
+*/
+
+console.log(accountState)
