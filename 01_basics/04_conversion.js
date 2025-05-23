44e450205229d9b04ce6e40f2ad2bf5035458c5c
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-05-23 16:37:32 +0000

    conversion

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 < 1);
+// console.log(2 != 1);
+// console.log(2 >= 1);
+
+// console.log("2" > 1);
+// console.log("01" > 1);
+
+
+// Try to avoid doing These tyoe of comparison
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);  
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===  strict comparison
+
+console.log("2" === 2);
+
+
+
