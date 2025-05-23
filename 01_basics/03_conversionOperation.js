f182310ce071425985e1e55359b6792f18e590da
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-05-23 13:49:44 +0000

    conversion of data Types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,27 +1,63 @@
-let score = false
+// let score = false
 
-console.log(typeof(score))
-console.log(typeof score)
+// console.log(typeof(score))
+// console.log(typeof score)
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 //"33" => 33
 //"33abc" => NaN
 //true => 1 false=> 0 
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true;  0 => false
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber); //string
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber); //string
+
+
+//****************Operations****************//
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(4/2);
+// console.log(9%5);
+// console.log(2**3); //8
+
+let str1 = "Saheli";
+let str2 = " Mahanty"
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log("1"+(2+2));
+console.log("1"+3);
+console.log(1+"3");
+console.log(3+2+"9");
+
+console.log(+true); //1   trick to convert
+console.log(+"");//0
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2   //try to avoid this 
+
+let gameCounter = 100n
+// ++gameCounter;
+console.log(++gameCounter);
