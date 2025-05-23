56358bbac73f3c81c0bf5f83d178a54abd7cc0c9
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-05-23 19:34:43 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,51 @@
+//  Primitive(Pass by value)
+
+//7 types : String, Number, Boolean ,null, undefined,Symbol,BigInt
+
+
+// JavaScript is dynamically typed, not statically typed.
+
+// 🔍 What does "dynamically typed" mean?
+// You don’t declare variable types. The type is determined at runtime based on the value.
+
+// A variable can hold any type of value at different times:
+
+
+let x = 10;      // x is a number
+x = "hello";     // now x is a string
+x = true;        // now x is a boolean
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 2345678909876234564567890098765n;
+console.log(typeof bigNumber);
+
+//Reference (Non Primitive)
+
+//Array,Objects,Functions
+
+const heroes = ["shaktiman","naagraj","doga"];
+
+let myObj = {
+    name:"hitesh",
+    age:22,
+}
+
+const myFunction = function(){
+    console.log("hello world\n")
+}
+myFunction();
+
+console.log(typeof outsideTemp)
+console.log(typeof null)
+console.log(typeof undefined);
+console.log(typeof myFunction);
+console.log(typeof heroes)
