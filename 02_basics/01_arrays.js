a7ef47e1808daeeb2ff551be4f7368aa491ce27e
Saheli Mahanty <sahelimahanty25@gmail.com>
2025-05-24 21:35:55 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,60 @@
+// array
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktimaan","naagraj"]
+console.log(myArr[0])
+//read from mdn
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+const myArr2 = new Array (1,3,8,9);
+// console.log(myArr2);
+
+// console.log(myArr2[3]);
+
+// Array Methods
+
+// myArr.push(6);
+// console.log(myArr);
+// myArr.push(7);
+// console.log(myArr);
+
+// myArr.push(78);
+// myArr.push(90);
+// console.log(myArr);
+
+// myArr.pop();
+// console.log(myArr);
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(89);
+// console.log(myArr);
+
+// myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes(9)); // return true if it is there in the array
+// console.log(myArr.indexOf(3)); //  return the index of the given element in the given array if does not exist in the array then returns -1
+
+// const newArr = myArr.join();
+
+// console.log(myArr)
+// console.log(newArr)
+// console.log(typeof newArr)
+
+//slice , splice
+const myar = new Array(0,1,2,3,4,5);
+console.log("A ",myar);
+
+const myn1 = myar.slice(1,3);
+
+console.log(myn1);
+console.log("B", myar);
+
+const myn2 = myar.splice(1,3);
+
+console.log(myn2);
+console.log("C",myar);
+
+
+
+
